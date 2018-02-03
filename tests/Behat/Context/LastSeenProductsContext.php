b44f702a53bbee52d72f5bf4bc0245f1a29db70f
Vladislav Sultanov <sultanov@solutiondrive.de>
2018-02-03 14:05:04 +0100

    Add context for lastSeenProduct

---
+++ tests/Behat/Context/LastSeenProductsContext.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by solutionDrive GmbH.
+ *
+ * @author   :  Vladislav Sultanov <sultanov@solutionDrive.de>
+ * @date     :  03.02.18
+ * @time     :  11:42
+ * @copyright:  2018 solutionDrive GmbH
+ */
+
+namespace Tests\AppBundle\Behat\Context;
+
+
+use Behat\Behat\Context\Context;
+use Sylius\Behat\Page\Admin\Customer\ShowPage;
+use Sylius\Component\Core\Model\ProductInterface;
+use Webmozart\Assert\Assert;
+
+class LastSeenProductsContext implements Context
+{
+    /**
+     * @var ShowPage
+     */
+    private $showPage;
+
+    public function __construct(ShowPage $showPage)
+    {
+        $this->showPage = $showPage;
+    }
+
+    /**
+     * @And customer :customer visited pages of products :product and :product
+     */
+    public function customerVistitedPagesOfProducts($customer, ...$products)
+    {
+        $lastSeenProduct = [];
+        /** @var ProductInterface $product */
+        foreach ($products as $product) {
+            $lastSeenProduct[] = $product->getCode();
+        }
+
+        $customer->setLastSeen($lastSeenProduct);
+    }
+
+    /**
+     * @Then I should see :count last seen products
+     */
+    public function iShouldSeeLastSeenProducts($count) {
+        $codes = $this->showPage->countLastSeenProductsCodes();
+        assertEquals($count, $codes);
+    }
+
+    /**
+     * @And I should see :name product
+     */
+    public function iShouldSeeThatProduct($name) {
+
+        assertTrue($this->showPage->hasLastSeenProduct($name));
+    }
+
+    /**
+     * @And I should not to see :name product
+     */
+    public function iShouldNotToSeeThatProduct($name) {
+        assertFalse($this->showPage->hasLastSeenProduct($name));
+    }
+}
---
+++ tests/Behat/ShowPage/ShowPage.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by solutionDrive GmbH.
+ *
+ * @author   :  Vladislav Sultanov <sultanov@solutionDrive.de>
+ * @date     :  03.02.18
+ * @time     :  12:14
+ * @copyright:  2018 solutionDrive GmbH
+ */
+
+namespace Tests\AppBundle\Behat\ShowPage;
+
+use Sylius\Behat\Page\Admin\Customer\ShowPage as BaseShowPage;
+
+class ShowPage extends BaseShowPage
+{
+    public function countLastSeenProductsCodes()
+    {
+        return count($this->getDocument()->find('css', '#LastSeenProducts .code'));
+    }
+
+    public function hasLastSeenProduct($product)
+    {
+        return null !== $this->getDocument()->find(
+            'css',
+            sprintf('#LastSeenProducts .code:contains("%s")', $product)
+        );
+    }
+}
